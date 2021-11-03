ddfbf8a43bb04ce2dac1b3c98942aa792fe691d0
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-03 18:28:44 +0200

    addTask 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const itemList = ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+
+  return itemEl;
+})
+
+ingredientsListEl.append(...itemList);
