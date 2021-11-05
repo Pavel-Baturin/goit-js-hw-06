44b291e7a6f5cbaf376134dde2236c552589de20
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-05 20:19:43 +0200

    addTask 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+     const formData = {
+        mail,
+        password,
+    };
+    
+
+    if (mail === "" || password === "") {
+        const message = "все поля должны быть заполнены";
+        alert(message);
+    } else {
+        console.log(formData);
+        
+        formEl.reset()
+    }
+}
