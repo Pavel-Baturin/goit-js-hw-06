d85485afa5ecd2f6a981a516f6bd2f02c94c3462
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-05 20:50:53 +0200

    addTask 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const randomColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+randomColorBtn.addEventListener('click', onRandomColorBtnClick)
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onRandomColorBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
 }
