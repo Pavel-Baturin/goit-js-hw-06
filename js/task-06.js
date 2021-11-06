ded7a3701d8072d9bf50f6c653aa709df225d1fe
Pavel-Baturin <pavel.baturin.92@gmail.com>
2021-11-06 19:25:28 +0200

    addCorrectionMentor

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,9 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur() {
-    let length = inputEl.value.length
-    if (length >= 6) {
+    let inputLength = inputEl.value.length;
+    const validLength = Number(inputEl.dataset.length);
+    if (inputLength === validLength) {
         inputEl.classList.replace('invalid', 'valid');
     } else {
         inputEl.classList.add('invalid');
